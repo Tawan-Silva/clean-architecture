92bfa6a57541f3e77deabb4ba46dabe128b1727f
Tawan Silva <tawan.tls43@gmail.com>
2023-03-01 23:50:30 -0300

    estrutura inicial

---
+++ src/test/java/com/tawandev/cleanarch/CleanArchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tawandev.cleanarch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanArchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
